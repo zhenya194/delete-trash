9ac58bbe7851531a36f90711140a8c0da7667d01
zhenya194 <zhenya.pro1753@gmail.com>
2026-02-27 22:00:42 +0200

    Update script.py

--- script.py
+++ script.py
@@ -8,3 +8,22 @@ delete_this_folders:list[str] = [
     ".ruff_cache",
     ".ipynb_checkpoints",
 ]
+
+for root, folders, files in os.walk("."):
+    for folder in folders:
+        if folder in delete_this_folders:
+            path = os.path.join(root, folder)
+            print(f"Deleting {path} ...")
+            shutil.rmtree(path)
+
+    for file in files:
+        if file.endswith(".pyc"):
+            path = os.path.join(root, file)
+            print(f"Deleting {path} ...")
+            os.remove(path)
+        elif file.endswith(".coverage"):
+            path = os.path.join(root, folder)
+            print(f"Deleting {path} ...")
+            shutil.rmtree(path)
+
+print("Completed.")
